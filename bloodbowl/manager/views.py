6b1f632ad461f71221cd955dca5a409949e05788
Anders Lauritsen <ducklord@gmail.com>
2014-02-23 18:24:51 +0100

    Fixed typo.

--- bloodbowl/manager/views.py
+++ bloodbowl/manager/views.py
@@ -2,6 +2,6 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from manager.models import Coach
 
-def coach_list(requst):
+def coach_list(request):
 	coach_list = Coach.objects.all().order_by('-name')
-	return render_to_response('manager/coach/list.html', RequestContext(requst, {'coach_list': coach_list}))
+	return render_to_response('manager/coach/list.html', RequestContext(request, {'coach_list': coach_list}))
