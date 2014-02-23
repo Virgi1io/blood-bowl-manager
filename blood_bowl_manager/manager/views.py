dc9e314a1ba88eb2f27ffc54e2869066157fd301
Anders Lauritsen <ducklord@gmail.com>
2014-02-23 17:59:06 +0100

    Added a simple coach list template to get the views started.

--- blood_bowl_manager/manager/views.py
+++ blood_bowl_manager/manager/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from manager.models import Coach
+
+def coach_list(requst):
+	coach_list = Coach.objects.all().order_by('-name')
+	return render_to_response('manager/coach/list.html', RequestContext(requst, {'coach_list': coach_list}))
