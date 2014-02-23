a4cb6f4e15278175034f29abc8b5d7447ba6477f
Anders Lauritsen <ducklord@gmail.com>
2014-02-23 18:59:04 +0100

    Added home page for the manager app.

--- bloodbowl/manager/urls.py
+++ bloodbowl/manager/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('manager.views',
+    url(r'^$', 'home'),
     url(r'^coaches/$', 'coach_list'),
 )
--- bloodbowl/manager/views.py
+++ bloodbowl/manager/views.py
@@ -2,6 +2,9 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from manager.models import Coach
 
+def home(request):
+    return render_to_response('manager/home/home.html', RequestContext(request))
+
 def coach_list(request):
 	coach_list = Coach.objects.all().order_by('-name')
 	return render_to_response('manager/coach/list.html', RequestContext(request, {'coach_list': coach_list}))
